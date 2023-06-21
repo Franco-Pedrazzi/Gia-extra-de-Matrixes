e8084caa668daa471e287965a81efe849fe0876e
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2023-06-21 20:01:00 -0300

    act 5

---
+++ script copy 3.js
@@ -0,0 +1,66 @@
+function RailFence(text,key){
+    let letras=text.length
+    let codigo=[];
+    let nros2
+    y1=0;
+    y2=key-1;
+    let buleano=false;
+ let nros=0;
+ for(let y=0;y<key && y<letras;y++){
+    codigo[y]=[];
+    console.log(codigo)
+}
+let xm=0;
+ for(let x=0;x<letras;x++){
+    console.log(4)
+
+ if(buleano==false){
+    
+ for(let y=0;y<key;y++){
+    if( xm!=letras){
+   
+    codigo[y][xm]=text[xm];
+
+    xm++
+   
+ 
+ }
+ else{
+ 
+ }
+}
+ if( xm!=letras){
+     buleano=true
+   
+    nros2=xm
+ }
+ }
+ 
+ if(buleano==true){
+     nros=0;
+ for(let y=key-2;y>=0;y--){
+    if( xm!=letras){
+ 
+    codigo[y][xm]=text[nros2];
+    xm++
+    nros2++
+   
+   
+ }
+ else{
+
+ }
+ }
+ if(xm!=letras){
+     buleano=false
+    
+ }
+ }
+
+ 
+ }
+ 
+}
+   
+  console.log( RailFence("19",10))
+ 
